5e1a87f0fb78356bd4acf0122e489153399cffd3
yyboo <xxx@163.com>
2025-05-26 10:11:50 +0800

    [chore] 1、修改打包方式；2、新增run.sh

--- pack.sh
+++ pack.sh
@@ -4,5 +4,5 @@ echo "编译可执行文件...."
 go build
 
 echo "开始打包..."
-cd ..
-tar -czvf gfast.tar gfast-mandate 
+# tar -czvf gfast.tar gfast-mandate 
+cd .. && zip -r gfast.zip gfast-mandate
--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ set -ex
 # echo "初始化数据库...."
 # mysql -uroot -pshangfei20250512 < ./resource/data/init.sql
 
-# echo "启动会议管理服务...."
-# nohup ./gfast &
+echo "启动会议管理服务...."
+nohup ./gfast &
